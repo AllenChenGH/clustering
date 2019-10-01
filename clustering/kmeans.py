dd0ebdbda7f796da7a3e8d66eb301198aaa79dcd
AllenChenGH <Allen_Cyfff@163.com>
2019-09-30 20:03:52 -0400

    submit for extra credit
    yufeng72@bu.edu

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -11,7 +11,21 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    element_number = len(points)
+    dimension_number = len(points[0])
+    center = []
+
+    for i in range(dimension_number):
+        center.append(0)
+
+    for i in range(element_number):
+        for j in range(dimension_number):
+            center[j] = center[j] + points[i][j]
+
+    for i in range(dimension_number):
+        center[i] = center[i]/element_number
+
+    return center
 
 
 def update_centers(data_set, assignments):
@@ -21,12 +35,25 @@ def update_centers(data_set, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+    element_number = len(data_set)
+    centers = []
+
+    max = 0
+    for assignment in assignments:
+        if (assignment > max):
+            max = assignment
+
+    for i in range(max + 1):
+        points = []
+        for j in range(element_number):
+            if (assignments[j] == i):
+                points.append(data_set[j])
+        centers.append(point_avg(points))
+
+    return centers
 
 
 def assign_points(data_points, centers):
-    """
-    """
     assignments = []
     for point in data_points:
         shortest = inf  # positive infinity
@@ -44,7 +71,13 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    dist = 0
+    dimension = len(a)
+
+    for i in range(dimension):
+        dist = dist + (a[i] - b[i]) * (a[i] - b[i])
+
+    return dist
 
 
 def generate_k(data_set, k):
@@ -52,15 +85,52 @@ def generate_k(data_set, k):
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    element_number = len(data_set)
+    centers = []
+    random = []
+
+    if (k > element_number - 1):
+        raise ValueError("k >= n!")
+
+    for i in range(k):
+        while (True):
+            temp = random.randint(0, element_number - 1)
+            if temp not in random:
+                random.append(temp)
+                break
+
+    for item in random:
+        centers.append(data_set[item])
+
+    return centers
 
 
 def get_list_from_dataset_file(dataset_file):
-    raise NotImplementedError()
+    file = open(dataset_file, "r")
+    reader = csv.reader(file)
+    data = []
+
+    for i in reader:
+        temp = []
+        temp.append(float(i[0]))
+        temp.append(float(i[1]))
+        data.append(temp)
+
+    return data
 
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    cost = 0
+
+    for label in clustering:
+        points = []
+        for point in clustering[label]:
+            points.append(point)
+        center = point_avg(points)
+        for point in clustering[label]:
+            cost = cost+distance(center,point)
+
+    return cost
 
 
 def k_means(dataset_file, k):
